c5724ec073768994409973f212db885678f1b580
Bhargavi Chiluka <138759748+bhargavich18@users.noreply.github.com>
2023-12-19 15:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.get("/", (request, response) => {
   return response.sendFile(path.resolve("./static/index.html"));
 });
 app.listen(process.env.PORT || 2000, () => {
-  console.log(`Server is running at port ${process.env.PORT || 2000}`);
+  console.log(`Server is up and running at port ${process.env.PORT || 2000}`);
 });
